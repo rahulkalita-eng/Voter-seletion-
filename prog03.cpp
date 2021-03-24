23c9e031828aaa7537da8d6f13b28755c79ed853
rahulkalita-eng <79743312+rahulkalita-eng@users.noreply.github.com>
2021-03-24 20:11:47 +0530

    Add files via upload

---
+++ prog03.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main() {
+int age;
+cout<<"Enter your age: ";
+cin>>age;
+if(age>=18) {
+    cout<<"Your are eligible to vote!!!!!"<<endl;
+} 
+else {
+    cout<<"Your are not eligible to vote !!!!!"<<endl;
+}
+return 0;
+}
